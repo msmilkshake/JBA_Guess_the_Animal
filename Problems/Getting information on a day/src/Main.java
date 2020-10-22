12c0bcada10b4f0ffe583fa9b7a59e22f1bd7ead
Paulo Filipe <prnfilipe@gmail.com>
2020-10-22 09:50:28 +0100

    Stage1

--- Problems/Getting information on a day/src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.time.LocalDate;
-import java.util.Scanner;
-
-class Main {
-    public static void main(String[] args) {
-        LocalDate date = LocalDate.parse(new Scanner(System.in).nextLine());
-        System.out.println(date.getDayOfYear() + " " + date.getDayOfMonth());
-    }
-}
