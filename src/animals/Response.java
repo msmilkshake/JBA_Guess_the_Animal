38d824ea9eb05d68db09658a21d1711f1d636cae
Paulo Filipe <prnfilipe@gmail.com>
2020-10-26 15:09:22 +0000

    Stage1 code cleanup

--- src/animals/Response.java
+++ src/animals/Response.java
@@ -1,17 +1,7 @@
 package animals;
 
 public enum Response {
-    NEGATIVE(-1),
-    NEUTRAL(0),
-    POSITIVE(1);
-    
-    private final double weight;
-    
-    Response(int weight) {
-        this.weight = weight;
-    }
-    
-    public double val() {
-        return weight;
-    }
+    NEGATIVE,
+    NEUTRAL,
+    POSITIVE
 }
